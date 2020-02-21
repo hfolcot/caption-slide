5296897b3a3b5af0b1851757166494b135772447
Heather Olcot <holcot@505games.com>
2020-02-21 15:20:52 +0000

    fixed bug with second slide

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 $(document).ready(function () {
-    $('#carouselExampleCaptions').on('slid.bs.carousel', function () {
-       $('.slide-fade-from-left').fadeIn();
-       $('.slide-fade-from-left2').fadeIn();
-       $('.slide-fade-from-left3').fadeIn();
-      })
+
 })
